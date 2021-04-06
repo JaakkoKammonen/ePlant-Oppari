64c6c5aae3f8829ce7340d8b728c34737eca4c3b
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2021-04-06 10:34:01 +0300

    koodin siistimistä

--- esp32/Kehitys/Ruukku/Ruukku.ino
+++ esp32/Kehitys/Ruukku/Ruukku.ino
@@ -17,58 +17,49 @@ long unsigned  int channel_id = 1079275;
 const String write_api_key ="CSI9579IPM1KPS7H";  
 const char* server = "api.thingspeak.com";
 
-
-#define INTERVAL 43200000     //12h   Vesipumpun millis() intervalli
-static uint64_t send_interval_ms; // maaritelty millis formaatti
+#define INTERVAL 43200000                                                       // 12h Vesipumpun millis() interval
+static uint64_t send_interval_ms;                                               // Määritelty millis-formaatti
 
 WebServer Server;
 AutoConnect Portal(Server);
 AutoConnectConfig config;
 WiFiClient wifiClient;
 
-/* 
- *  Field4= 0/1 vesipumppu pois/päällä 3sec
- *  Field5= 0/1 anturien arvojen lähetys thingSpeak pois/päällä
- *  Field6= 0/1 automaattiohjaus pois/päällä
- *  
- *  MenuItems: https://hieromon.github.io/AutoConnect/apiconfig.html#menuitems
-*/
+int ilmaPumppu = 33;                                                            // Ilmapumpun ohjauspinni (Releen ohjaus)
 
-int ilmaPumppu = 33;                 // Ilmapumpun ohjauspinni eli Releen ohjaus
-
-int PHTasoAnturiVirta = 5;          // Antureiden virrat
+int PHTasoAnturiVirta = 5;                                                      // Antureiden virrat
 int PHAnturi= 32;
 
 int ECAnturiVirta = 18;
 int ECAnturi= 35;
  
-int ilmaPumppuThingspeakOhjaus = 4; // Thingpeakin Read-metodilla päivitettävä arvo 0/1
-int anturienArvo = 5;               // Thingspeak Read-metodilla päivitettävä arvo 0/1 
+int ilmaPumppuThingspeakOhjaus = 4;                                             // Thingpeakin Read-metodilla päivitettävä arvo 0/1
+int anturienArvo = 5;                                                           // Thingspeak Read-metodilla päivitettävä arvo 0/1 
 int moistureRead = 0; 
 
 int moistureField = 2;
-int ThingSpeakFieldIlmaPumppu = 3; // Thingspeak-field numero
-int ThingSpeakFieldAnturiDataSend = 5;             // Thingspeak field arvo
-int automaattiOhjausField = 6;     // Thingspeak field arvo
+int ThingSpeakFieldIlmaPumppu = 3;                                              // Thingspeak-field numero
+int ThingSpeakFieldAnturiDataSend = 5;                                          // Thingspeak field arvo
+int automaattiOhjausField = 6;                                                  // Thingspeak field arvo
 
 void setup() {
 
-  pinMode(ilmaPumppu,OUTPUT);             // PinOutput anturien pinneille
-  pinMode(PHTasoAnturiVirta,OUTPUT);    // ja releelle
+  pinMode(ilmaPumppu,OUTPUT);                                                   // Pin-Output anturien pinneille
+  pinMode(PHTasoAnturiVirta,OUTPUT);                                            // Relay anturien pinneille
   pinMode(ECAnturiVirta,OUTPUT);
 
   delay(1000);
-  config.apid = "ePlantVol2";             // Hotspot-nimi
-  config.psk  = "ePlantVersion";          // Salasana
-  config.portalTimeout = 60000;           // 60 sec Hotspot 
-  config.title ="ePlant";                 // Avattavan Hotspot-nettisivu title
-  config.retainPortal = true;             // Hotspots koko ajan paalla
-  config.ota = AC_OTA_BUILTIN;            // Over the air -paivitys
-  config.menuItems =                      // Nettisivun komponetit
+  config.apid = "ePlantVol2";                                                   // Hotspot-nimi
+  config.psk  = "ePlantVersion";                                                // Salasana
+  config.portalTimeout = 60000;                                                 // 60 sec Hotspot 
+  config.title ="ePlant";                                                       // Avattavan Hotspot-nettisivu title
+  config.retainPortal = true;                                                   // Hotspots koko ajan paalla
+  config.ota = AC_OTA_BUILTIN;                                                  // Over-The-Air -päivitys
+  config.menuItems =                                                            // Nettisivun komponetit
      AC_MENUITEM_CONFIGNEW
      ;
-  Portal.config(config);                  // Wifi avaa web portaalin
-  Serial.begin(115200);                   // Serial monitor
+  Portal.config(config);                                                        // Wifi avaa web-portaalin
+  Serial.begin(115200);                                                         // Serial monitor
   Serial.println();
 
   if (Portal.begin()) {
@@ -76,16 +67,16 @@ void setup() {
     Serial.println("WiFi connected: " + WiFi.localIP().toString());
     
   }
-WiFi.setAutoReconnect(true);             // Automaatti wifi yhdistys, jos yhteys katkeaa
-ThingSpeak.begin(wifiClient);            // Alustus Thingspeakille lahetykseen
+WiFi.setAutoReconnect(true);                                                    // Automaattinen Wifin yhdistys, jos yhteys katkeaa
+ThingSpeak.begin(wifiClient);                                                   // Alustus Thingspeakille datan lähetykseen
 
-send_interval_ms = millis();            // ajan aloitus
+send_interval_ms = millis();                                                    // Ajan aloitus
 
 }
 
  static int ECAnturiArvo() {
 
-        digitalWrite(ECAnturiVirta, HIGH);             // Virrat päälle, odotus, luetaan, tallennetaan muuttujaan tieto, virrat pois
+        digitalWrite(ECAnturiVirta, HIGH);                                      // Virrat päälle -> odotus -> luetaan -> tallennetaan muuttujaan tieto -> virrat pois
         delay(5000);
         
         int ECTaso1 = analogRead(ECAnturi);
@@ -111,7 +102,7 @@ send_interval_ms = millis();            // ajan aloitus
 
   static  int PHTasoAnturiArvo() {
   
-        digitalWrite(PHTasoAnturiVirta, HIGH);                // Virrat päälle, odotus, luetaan, tallennetaan muuttujaan tieto, virrat pois
+        digitalWrite(PHTasoAnturiVirta, HIGH);                                   // Virrat päälle -> odotus -> luetaan -> tallennetaan muuttujaan tieto -> virrat pois
         delay(5000);
         
         int PHTaso1 = analogRead(PHAnturi);
@@ -140,21 +131,21 @@ void loop() {
   
   Portal.handleClient();
 
-  if (WiFi.status() != WL_CONNECTED) {            // Wifi ei ole kytketty
+  if (WiFi.status() != WL_CONNECTED) {                                           // Wifi ei ole kytketty
       
   // Pidetään ilmapummpua päällä
   Serial.println("Ei nettiä, ilmapumppu päällä kokoajan.");
-  digitalWrite(ilmaPumppu,HIGH);                  // Ilmapumppu päälle eli Releen pinni aktivoidaan
+  digitalWrite(ilmaPumppu,HIGH);                                                 // Ilmapumppu päälle -> Releen pinni aktivoidaan
 
   }
 
   
-   if (WiFi.status() == WL_CONNECTED) {                                                  // Wifi kytkettyna
+   if (WiFi.status() == WL_CONNECTED) {                                          // Wifi kytkettynä
  
-      ilmaPumppuThingspeakOhjaus = ThingSpeak.readFloatField(channel_id, ThingSpeakFieldIlmaPumppu);      // Luetaan ilmapumpunohjausarvo Thingspeakista
+      ilmaPumppuThingspeakOhjaus = ThingSpeak.readFloatField(channel_id, ThingSpeakFieldIlmaPumppu);   // Luetaan ilmapumpunohjausarvo Thingspeakista
         
-      if(ilmaPumppuThingspeakOhjaus == 1){                                                           // Jos arvo 1 == pumppu paalla 3sec
-        digitalWrite(ilmaPumppu, HIGH);                                                  // Jos arvo 0 == pumppu ei paalla
+      if(ilmaPumppuThingspeakOhjaus == 1){                                       // Jos arvo 1 == pumppu päällä 3 sec
+        digitalWrite(ilmaPumppu, HIGH);                                          // Jos arvo 0 == pumppu ei ole päällä
         Serial.println("ThingSpeak arvo 1. Ilmapumppu on päällä. ");
       }
       else if(ilmaPumppuThingspeakOhjaus == 0){
@@ -163,16 +154,16 @@ void loop() {
       }
      
 
-      anturienArvo = ThingSpeak.readFloatField(channel_id, ThingSpeakFieldAnturiDataSend);             // Luetaan anturien lahetys ohjausarvo thingspeak
+      anturienArvo = ThingSpeak.readFloatField(channel_id, ThingSpeakFieldAnturiDataSend);   // Luetaan anturien lähetyksen ohjausarvo Thingspeakiin
     
-       if (anturienArvo == 1) {                                                         // Mikali arvo 1 == luetaan anturit ja tallennetaan
-                                                                                        // metodin sisaisiin muuttujiin. Sitten ne lahetetaan Thingspeak
+       if (anturienArvo == 1) {                                                  // Mikäli arvo 1 == luetaan anturit ja tallennetaan
+                                                                                 // metodin sisäisiin muuttujiin ja lähetetään Thingspeakiin
            int PHarvo = PHTasoAnturiArvo();
            int ECarvo = ECAnturiArvo();
            
-        if (wifiClient.connect(server, 80))                                             // api.thingspeak.com
-            {                                                                           // Thingspeak tunneli avataan
-              String data_to_send = write_api_key;                                      // Apikey
+        if (wifiClient.connect(server, 80))                                      // api.thingspeak.com
+            {                                                                    // Thingspeak tunneli avataan
+              String data_to_send = write_api_key;                               // API-key
               data_to_send += "&field1=";
               data_to_send += PHarvo;
               data_to_send += "&field2=";
@@ -191,12 +182,12 @@ void loop() {
               delay(1000);
               Serial.println("ThingSpeak arvo 1. Anturit ovat luettu ja lähetetty!");
   }
-  wifiClient.stop();                                                                     // Thingspeak tunneli suljetaan
+  wifiClient.stop();                                                            // Thingspeak tunneli suljetaan
   
        }
 
-       else {                                                         // Kun anturien arvo on 0
-        Serial.println("ThingSpeak arvo 0. Anturien arvoja ei lähetetty");              // Tulostetaan consoleen
+       else {                                                                   // Kun anturien arvo on 0
+        Serial.println("ThingSpeak arvo 0. Anturien arvoja ei lähetetty");      // Tulostetaan consoleen
        }
 
 
