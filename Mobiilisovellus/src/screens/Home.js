de98784af05f2f2ffcb09b24747f1272fd80368f
Minna Kuivalainen <bgg244@myy.haaga-helia.fi>
2021-04-20 15:04:09 +0300

    ScrollView ongelma korjattu

--- Mobiilisovellus/src/screens/Home.js
+++ Mobiilisovellus/src/screens/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Image, FlatList, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import firebase from '../components/firebase';
 import { Snackbar } from 'react-native-paper';
 
