72f0ddbf888c95f49f13093fb60684e693ada0a8
Minna Kuivalainen <bgg244@myy.haaga-helia.fi>
2021-05-25 11:21:25 +0300

    User -visuaalisuus, Kuvan lisäys, ThingSpeak Input, edit ThingSpeak info jne

--- Mobiilisovellusvol2/ePlant/src/screens/settings/Edit-ePlantPot.js
+++ Mobiilisovellusvol2/ePlant/src/screens/settings/Edit-ePlantPot.js
@@ -1,71 +1,198 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import LogginMiddleware from "../../components/Redux/03-middleware/LogginMiddleware";
-import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { Input, Button } from "react-native-elements";
-import { FancyAlert } from 'react-native-expo-fancy-alerts';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Ionicons, MaterialIcons, AntDesign } from "@expo/vector-icons";
+import { Input, Button, Card } from "react-native-elements";
 import FireBasemiddleware from "../../components/Redux/03-middleware/FireBasemiddleware";
 
 export default function Login(props) {
   const [ePlantPot, setEPlantPot] = useState({
     channel_id: props.navigation.state.params.ePlantPot.channel_id,
-    write_apikey:props.navigation.state.params.ePlantPot.write_apikey,
-    ePlantID: props.navigation.state.params.ePlantID
+    write_apikey: props.navigation.state.params.ePlantPot.write_apikey,
+    ePlantID: props.navigation.state.params.ePlantID,
   });
   const [new_ePlantPot, setNewEPlantPot] = useState({
     channel_id: props.navigation.state.params.ePlantPot.channel_id,
-    write_apikey:props.navigation.state.params.ePlantPot.write_apikey,
-    ePlantID: props.navigation.state.params.ePlantID
+    write_apikey: props.navigation.state.params.ePlantPot.write_apikey,
+    ePlantID: props.navigation.state.params.ePlantID,
   });
 
-  const navigate = props.navigation.state.params.navigate
+  const navigate = props.navigation.state.params.navigate;
 
   //console.log(props.navigation.state.params)
   //console.log(new_ePlantPot)
-  
+
   const EditePlant = () => {
-    FireBasemiddleware.ModifyUserEPlant(new_ePlantPot, navigate)
-  }
+    FireBasemiddleware.ModifyUserEPlant(new_ePlantPot, navigate);
+  };
   return (
-    <View style={styles.container}>
-
-    <TouchableOpacity onPress={() => navigate('Settings')}>
+    <ScrollView style={styles.container}>
+      <TouchableOpacity onPress={() => navigate("Settings")}>
         <Ionicons name="arrow-back-outline" size={30} style={styles.arrow} />
-    </TouchableOpacity>
-
-    <Text>
-        Now your channel id is: {ePlantPot.channel_id}
-    </Text>
-      <Input
-        placeholder="New channel id"
-        type="text"
-        required={true}
-        inputStyle={styles.signintext}
-        onChangeText={(text) => setNewEPlantPot({...new_ePlantPot, channel_id: text})}
-      />
-
-    <Text>
-        Now your write_apikey is: {ePlantPot.write_apikey}
-    </Text>
-      <Input
-        placeholder="New write_apikey"
-        required={true}
-        inputStyle={styles.signintext}
-        onChangeText={ (text) => setNewEPlantPot({...new_ePlantPot, write_apikey: text})}
-      />
-    
-    <Button
-    title= "Edit"
-    onPress={() => EditePlant()}
-    />
-    </View>
+      </TouchableOpacity>
+      <View>
+        <Text style={styles.headerText}>Edit Pot Settings</Text>
+      </View>
+      <View style={styles.container2}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/thingspeak.png")}
+        />
+        <View style={styles.currentdatawrapper}>
+        <Text style={styles.currentchannel}> Current channel ID: <Text style={styles.currentchanneldata}>{ePlantPot.channel_id}</Text> </Text>
+        <Text style={styles.currentapikey}> Current Write API-key: <Text style={styles.currentapikeydata}>{ePlantPot.write_apikey}</Text></Text>
+        </View>
+        <View style={styles.titlewrapper}>
+          <Card.Title>EDIT CHANNEL ID</Card.Title>
+        </View>
+        <Input
+          placeholder=" New Channel ID"
+          type="text"
+          required={true}
+          inputStyle={styles.channelidinput}
+          leftIcon={<AntDesign name="areachart" size={24} color="grey" />}
+          onChangeText={(text) =>
+            setNewEPlantPot({ ...new_ePlantPot, channel_id: text })
+          }
+        />
+        <View style={styles.titlewrapper}>
+          <Card.Title>EDIT WRITE API-KEY</Card.Title>
+        </View>
+        <Input
+          placeholder=" New Write API-key"
+          clearButtonMode="always"
+          required={true}
+          inputStyle={styles.apikeyinput}
+          leftIcon={<MaterialIcons name="vpn-key" size={24} color="grey" />}
+          onChangeText={(text) =>
+            setNewEPlantPot({ ...new_ePlantPot, write_apikey: text })
+          }
+        />
+        <View style={styles.buttonwrapper}>
+          <Button
+            title="Save"
+            buttonStyle={styles.buttonthingspeak}
+            onPress={() => EditePlant()}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
+    padding: 3,
+    flex: 2,
+  },
+  container2: {
+    marginBottom: 3,
     padding: 50,
-    flex: 1,
+  },
+  header: {
+    shadowColor: "#DEDDDD",
+    shadowOpacity: 2,
+    shadowOffset: {
+      height: 2,
+      width: 2,
+    },
+    elevation: 4,
+    backgroundColor: "#FAFAFA",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "600",
+    marginBottom: 20,
+    color: "#63816D",
+    alignSelf: "center",
+  },
+  headerText: {
+    fontSize: 22,
+    marginTop: 20,
+    fontWeight: "600",
+    marginBottom: 20,
+    color: "#63816D",
+    alignSelf: "center",
+  },
+  arrow: {
+    marginLeft: 20,
+    marginTop: 10,
+    color: "grey",
+  },
+  currentchannel: {
+    fontSize: 14,
+    fontWeight: "bold",
+    letterSpacing: 0.5,
+  },
+  currentchanneldata: {
+    fontSize: 13,
+    fontWeight: "normal",
+    letterSpacing: 0.5,
+  },
+  currentapikey: {
+    fontSize: 14,
+    fontWeight: "bold",
+    letterSpacing: 0.5,
+    marginLeft: 33
+  },
+  currentapikeydata: {
+    fontSize: 13,
+    fontWeight: "normal",
+    letterSpacing: 0.5,
+    marginLeft: 33
+  },
+  logo: {
+    width: 260,
+    height: 210,
+  },
+  titlewrapper: {
+    marginTop: 20,
+  },
+  buttonthingspeak: {
+    backgroundColor: "#63816D",
+    borderRadius: 3,
+    marginTop: 10,
+    fontSize: 12,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+    width: 100,
+    height: 40,
+  },
+  buttonwrapper: {
     alignItems: "center",
   },
-  
+  currentdatawrapper: {
+    alignItems: "center",
+  },
+  apikeytext: {
+    fontSize: 12,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginTop: 20,
+    letterSpacing: 0.5,
+  },
+  channelidtext: {
+    fontSize: 12,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+  },
+  apikeyinput: {
+    fontSize: 12,
+    fontWeight: "normal",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+  },
+  channelidinput: {
+    fontSize: 12,
+    fontWeight: "normal",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+  },
 });
--- Mobiilisovellusvol2/ePlant/src/screens/settings/User.js
+++ Mobiilisovellusvol2/ePlant/src/screens/settings/User.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useSelector } from 'react-redux';
 import { Button } from "react-native-elements";
@@ -14,8 +14,8 @@ export default function UserNotification(props) {
     const DeleteUser = () => {
 
         swal({
-            title: "Delete user?",
-            text: "Are you sure? This will delete user and all user data from databases!",
+            title: "Delete your account?",
+            text: "Are you sure? This will delete your account and all of your data from databases!",
             icon: "error",
             buttons: [true, "Do it!"],
             dangerMode: true,
@@ -37,10 +37,8 @@ export default function UserNotification(props) {
                     LogginMiddleware.DeleteUser(user, password, navigate)
                   });
             
-
-            
             } else {
-              swal("Your plant is safe!", {
+              swal("Your account is safe!", {
                 button: "Close",
                 timer: 1500,
               });
@@ -50,39 +48,40 @@ export default function UserNotification(props) {
     }
     const UserInfo = () => {
         return (
-            <View>
-               <Text>
-                   User name: {user.displayName}
-                </Text> 
-            
-                <Text>
-                   User email: {user.email}
-                </Text>
-
+            <View style={styles.container}>
+            <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigate('Settings')}>
+                    <Ionicons name="arrow-back-outline" size={30} style={styles.arrow} />
+                </TouchableOpacity></View>
+            <Image style={styles.avatar} source={require("../../assets/userprofile.png")}/>
+            <View style={styles.body}>
+              <View style={styles.bodycontent}>
+              <Text style={styles.name}>{user.displayName}</Text>
+                <Text style={styles.email}>{user.email}</Text>
+                <View style={styles.buttoncontainer}>
                 <Button
-                title="Edit user"
+                title="Edit username"
+                buttonStyle={styles.userbutton}
                 onPress={() => navigate("EditUser", {user: user, navigate:navigate})}
-                />
+                />  
+                </View>      
+                <View style={styles.buttoncontainer}>
                 <Button
-                title="Delete all user data"
+                buttonStyle={styles.userbutton}
+                title="Delete your account"
                 onPress={() => DeleteUser()}
                 />
-
-
+                </View>
+            <View>
+            </View>
+            </View>
+            </View>
             </View>
         )
     }
     return (
         <ScrollView style={styles.container}>
-            
-            <View>
-                <TouchableOpacity onPress={() => navigate('Settings')}>
-                    <Ionicons name="arrow-back-outline" size={30} style={styles.arrow} />
-                </TouchableOpacity>
-                <Text style={styles.description}>Tähän käyttäjän omat ilmoitusasetukset</Text>
-                
                 {UserInfo()}
-            </View>
         </ScrollView>
     );
 };
@@ -105,5 +104,74 @@ const styles = StyleSheet.create({
         marginLeft: 20,
         marginTop: 10,
         color: 'grey'
-    }
+    },
+    header:{
+        shadowColor: "#DEDDDD",
+        backgroundColor: "#FAFAFA",
+        shadowOpacity: 2,
+        shadowOffset: {
+            height: 1,
+            width: 1,
+          },
+        height:200,
+      },
+      avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: "white",
+        marginBottom:10,
+        alignSelf:'center',
+        position: 'absolute',
+        marginTop:130
+      },
+      name:{
+        fontSize:22,
+        color:"#FFFFFF",
+        fontWeight:'600',
+      },
+      body:{
+        marginTop:40,
+      },
+      bodycontent: {
+        flex: 1,
+        alignItems: 'center',
+        padding:30,
+      },
+      name:{
+        fontSize:28,
+        color: "#696969",
+        fontWeight: "600"
+      },
+      email:{
+        fontSize:16,
+        marginTop: 10,
+        fontStyle: "italic",
+        color: "#63816D", 
+        marginBottom: 5
+      },
+      description:{
+        fontSize:16,
+        color: "#696969",
+        marginTop:10,
+        textAlign: 'center'
+      },
+      buttoncontainer: {
+        marginTop: 10,
+        width: 100,
+        height: 40,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+      userbutton: {
+        backgroundColor: "#63816D",
+        borderRadius: 3,
+        fontSize: 12,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: 0.5,
+      }
 });
