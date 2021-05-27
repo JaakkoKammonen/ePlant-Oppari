6e8464ccc357fd81d2e992748b9c0ec9e1135c75
Jaakko Kammonen <jaakko.kammonen@hotmail.com>
2021-05-27 22:19:11 +0300

    PH testailua

--- esp32/Kehitys/Ruukku/Ruukku.ino
+++ esp32/Kehitys/Ruukku/Ruukku.ino
@@ -44,12 +44,11 @@ int AirPump = 33;
 int AirPumpThingSpeakField = 3;
 
 
-int PHSensor= 32;
-float calibration_value = 21.34 - 0.7;
-int phval = 0; 
-unsigned long int avgval; 
-int buffer_arr[10],temp;
-float ph_act;
+int PHSensor= 35; 
+int sensorValue = 0; 
+unsigned long int avgValue; 
+float b;
+int buf[10],temp;
 
 int ECAnturiVirta = 18;
 int ECAnturi= A0;
@@ -87,33 +86,7 @@ ThingSpeak.begin(wifiClient);                                                   
 }
 
 
- int getMedianNum(int bArray[], int iFilterLen)
-                  {
-                  int bTab[iFilterLen];
-                  for (byte i = 0; i<iFilterLen; i++)
-                  bTab[i] = bArray[i];
-                  int i, j, bTemp;
-                  for (j = 0; j < iFilterLen - 1; j++)
-                  {
-                  for (i = 0; i < iFilterLen - j - 1; i++)
-                  {
-                  if (bTab[i] > bTab[i + 1])
-                  {
-                  bTemp = bTab[i];
-                  bTab[i] = bTab[i + 1];
-                  bTab[i + 1] = bTemp;
-                  }
-                  }
-                  }
-                  if ((iFilterLen & 1) > 0)
-                  bTemp = bTab[(iFilterLen - 1) / 2];
-                  else
-                  bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-                  return bTemp;
-                  }
-
-
-            
+ 
  static int ECAnturiArvo() {
 
         digitalWrite(ECAnturiVirta, HIGH);                                      // Virrat päälle -> odotus -> luetaan -> tallennetaan muuttujaan tieto -> virrat pois
@@ -126,16 +99,16 @@ ThingSpeak.begin(wifiClient);                                                   
 
           for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
           analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
-          averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF/ 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+          averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF/ 4095; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
           float compensationCoefficient=1.0+0.02*(temperature-25.0); //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
           float compensationVolatge=averageVoltage/compensationCoefficient; //temperature compensation
           tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
           //Serial.print("voltage:");
           //Serial.print(averageVoltage,2);
           //Serial.print("V ");
-          Serial.print("TDS Value:");
-          Serial.print(tdsValue,0);
-          Serial.println("ppm");
+          //Serial.print("TDS Value:");
+          //Serial.print(tdsValue,0);
+          //Serial.println("ppm");
 
         digitalWrite(ECAnturiVirta,LOW);
         int returnValue = tdsValue;
@@ -146,33 +119,36 @@ ThingSpeak.begin(wifiClient);                                                   
   static  int PHTasoAnturiArvo() {
         
                 for(int i=0;i<10;i++) 
-               { 
-               buffer_arr[i]=analogRead(PHSensor);
-               delay(30);
-               }
-               for(int i=0;i<9;i++)
-               {
-               for(int j=i+1;j<10;j++)
-               {
-               if(buffer_arr[i]>buffer_arr[j])
-               {
-               temp=buffer_arr[i];
-               buffer_arr[i]=buffer_arr[j];
-               buffer_arr[j]=temp;
-               }
-               }
-               }
-               avgval=0;
-               for(int i=2;i<8;i++)
-               avgval+=buffer_arr[i];
-               float volt=(float)avgval*5.0/1024/6; 
-                ph_act = -5.70 * volt + calibration_value;
-               
-               Serial.println("pH Val: ");
-               Serial.print(ph_act);
-               delay(1000);
+                 { 
+                  buf[i]=analogRead(PHSensor);
+                  delay(10);
+                 }
+                 for(int i=0;i<9;i++)
+                 {
+                  for(int j=i+1;j<10;j++)
+                  {
+                   if(buf[i]>buf[j])
+                   {
+                    temp=buf[i];
+                    buf[i]=buf[j];
+                    buf[j]=temp;
+                   }
+                  }
+                 }
+                 avgValue=0;
+                 for(int i=2;i<8;i++)
+                 avgValue+=buf[i];
+                
+                  float pHVol=(float)avgValue/6*4.95/4095;
+                  //Serial.print("v = ");
+                  //Serial.println(pHVol);
+                
+                  float phValue = -5.70 * pHVol + 21.34;    
+                  //float phValue = 7 + ((2.5 - pHVol) / 0.18);
+                  //Serial.print("Ph=");
+                  //Serial.println(phValue);
 
-          float returnValue = ph_act;   
+          float returnValue = phValue;   
         
          return returnValue;
  };
@@ -256,4 +232,33 @@ void loop() {
   Serial.println("Loop over");
   delay(10000);     // Odotetaan 10 sekuntia
  }
+
+
+          int getMedianNum(int bArray[], int iFilterLen){
+  
+                  int bTab[iFilterLen];
+                  for (byte i = 0; i<iFilterLen; i++)
+                  bTab[i] = bArray[i];
+                  int i, j, bTemp;
+                  for (j = 0; j < iFilterLen - 1; j++)
+                  {
+                  for (i = 0; i < iFilterLen - j - 1; i++)
+                  {
+                  if (bTab[i] > bTab[i + 1])
+                  {
+                  bTemp = bTab[i];
+                  bTab[i] = bTab[i + 1];
+                  bTab[i + 1] = bTemp;
+                  }
+                  }
+                  }
+                  if ((iFilterLen & 1) > 0)
+                  bTemp = bTab[(iFilterLen - 1) / 2];
+                  else
+                  bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+                  return bTemp;
+               }
+
+
+            
  
--- esp32/Tests/PH sensor/phtesti/phtesti.ino
+++ esp32/Tests/PH sensor/phtesti/phtesti.ino
@@ -1,17 +1,42 @@
+const int analogInPin = 35; 
+int sensorValue = 0; 
+unsigned long int avgValue; 
+float b;
+int buf[10],temp;
+void setup() {
+ Serial.begin(115200);
+}
+ 
+void loop() {
+ for(int i=0;i<10;i++) 
+ { 
+  buf[i]=analogRead(analogInPin);
+  delay(10);
+ }
+ for(int i=0;i<9;i++)
+ {
+  for(int j=i+1;j<10;j++)
+  {
+   if(buf[i]>buf[j])
+   {
+    temp=buf[i];
+    buf[i]=buf[j];
+    buf[j]=temp;
+   }
+  }
+ }
+ avgValue=0;
+ for(int i=2;i<8;i++)
+ avgValue+=buf[i];
 
-int pH_Value; 
-float Voltage;
+  float pHVol=(float)avgValue/6*4.95/4095;
+  Serial.print("v = ");
+  Serial.println(pHVol);
+
+  float phValue = -5.70 * pHVol + 21.34;    
+  //float phValue = 7 + ((2.5 - pHVol) / 0.18);
+  Serial.print("Ph=");
+  Serial.println(phValue);
  
-void setup() 
-{ 
-  Serial.begin(115200);
-  pinMode(pH_Value, INPUT); 
-} 
- 
-void loop() 
-{ 
-  pH_Value = analogRead(32); 
-  Voltage = pH_Value * (4.94 / 1023.0);
-  Serial.println(Voltage); 
-  delay(500); 
+ delay(20);
 }
