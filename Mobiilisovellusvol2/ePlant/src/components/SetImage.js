ee2f83931a79d39b67d27db85bf9715c01452b86
Minna Kuivalainen <bgg244@myy.haaga-helia.fi>
2021-06-01 19:31:16 +0300

    SetImage (Unsplash)

--- Mobiilisovellusvol2/ePlant/src/components/SetImage.js
+++ Mobiilisovellusvol2/ePlant/src/components/SetImage.js
@@ -1,17 +1,15 @@
 // importing all images and naming them
-import dill from '../assets/set_image/tilli.jpg'
-import parsley from '../assets/set_image/persilja.jpg'
-import caraway from '../assets/set_image/kumina.jpg'
-import thyme from '../assets/set_image/timjami.jpg'
-import basil from '../assets/set_image/basilika.jpg'
-import coriander from '../assets/set_image/korianteri.jpg'
-import bloodydock from '../assets/set_image/viinisuolaheina.jpg'
+import dill from '../assets/set_image/dill.jpg'
+import parsley from '../assets/set_image/parsley.jpg'
+import caraway from '../assets/set_image/caraway.jpg'
+import thyme from '../assets/set_image/thyme.jpg'
+import basil from '../assets/set_image/basil.jpg'
+import coriander from '../assets/set_image/coriander.jpg'
 import oregano from '../assets/set_image/oregano.jpg'
-import chive from '../assets/set_image/ruohosipuli.jpg'
-import mint from '../assets/set_image/minttu.jpg'
-import tarragon from '../assets/set_image/rakuuna.jpg'
-import lemonbalm from '../assets/set_image/sitruunamelissa.jpg'
-import notdefinedImage from '../assets/set_image/ei_maaritelty.jpg'
+import chive from '../assets/set_image/chive.jpg'
+import mint from '../assets/set_image/mint.jpg'
+import lemonbalm from '../assets/set_image/lemon_balm.jpg'
+import notdefinedImage from '../assets/set_image/other.jpg'
 import placeholderImage from "../assets/set_image/placeholderImage.png"
 
 // selecting which image to return based on param from Search.js
@@ -32,16 +30,12 @@ export default function SetBackgroundImg(plant) {
             return basil;
         case 'coriander':
             return coriander;
-        case 'bloody dock':
-            return bloodydock;
         case 'oregano':
             return oregano;
         case 'chive':
             return chive;
         case 'mint':
             return mint;
-        case 'tarragon':
-            return tarragon;
         case 'lemon balm':
             return lemonbalm;
         case 'other':
